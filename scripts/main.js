1ebdd39eeb6317534cd43c09ca09c502780d8aa2
jmanock <jonmanock@gmail.com>
2016-07-26 01:57:37 -0400

    break time

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
 (function(){
   'use strict';
+
+  var messageForm = document.getElementById('message-form');
+  var messageInput = document.getElementById('new-post-message');
+  var titleInput = document.getElementById('new-post-title');
+  var signInButton = document.getElementById('sign-in-button');
+  var signOutButton = document.getElementById('sign-out-button');
+  var splashPage = document.getElementById('page-splash');
+  var addPost = document.getElementById('add-post');
+  var addButton = document.getElementById('add');
+  var recentPostsSection = document.getElementById('recent-posts-list');
+  var userPostsSection = document.getElementById('user-posts-list');
+  var topUserPostsSection = document.getElementById('top-user-posts-list');
+  var recentMenuButton = document.getElementById('menu-recent');
+  var myPostsMenuButton = document.getElementById('menu-my-posts');
+  var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
 })();
